4578bcc8fb53ae6d73443d7c83c056e541ce4ed8
theleozera <lpoliveira132@gmail.com>
2021-07-06 16:04:56 -0300

    123

--- src/main/java/ifrs/dev2/buyer/api/ClasseController.java
+++ src/main/java/ifrs/dev2/buyer/api/ClasseController.java
@@ -6,7 +6,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
- //ok2
+ //ok2sdfhjklds
 @RestController
 @RequestMapping(path="/api/classe")
 public class ClasseController  {
