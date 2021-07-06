aba39e914e81ab898c2a39a0d0f5fa9f5d905dc6
Fernando Rodrigues <34346597+nandolrs@users.noreply.github.com>
2021-07-05 23:29:49 -0300

    .

--- src/main/java/ifrs/dev2/buyer/api/ProdutoController.java
+++ src/main/java/ifrs/dev2/buyer/api/ProdutoController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.List; // fernando
 
 @RestController
 @RequestMapping(path="/api/produto")
