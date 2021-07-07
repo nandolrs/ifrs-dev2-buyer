c5cb3a9c0c21cd22461f0353c422619530ee3c7b
theleozera <lpoliveira132@gmail.com>
2021-07-06 22:01:58 -0300

    dados_unidade_de_medida criado, unidade_de_medida_repositoria criado e controler unidade de medida em andamento

--- src/main/java/ifrs/dev2/buyer/api/ClasseController.java
+++ src/main/java/ifrs/dev2/buyer/api/ClasseController.java
@@ -6,7 +6,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
- //ok2sdfhjklds
+ //ok2sdfhjkldsok
 @RestController
 @RequestMapping(path="/api/classe")
 public class ClasseController  {
---
+++ src/main/java/ifrs/dev2/buyer/api/UnidadeDeMedidaController.java
@@ -0,0 +1,36 @@
+package ifrs.dev2.buyer.api;
+
+import ifrs.dev2.buyer.dados.Classe;
+import ifrs.dev2.buyer.dados.DadoInterface;
+import ifrs.dev2.buyer.dados.UnidadeDeMedida;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.Id;
+import javax.print.attribute.standard.Media;
+import java.util.List;
+
+@RestController // This means that this class is a Controller
+@RequestMapping(path="/api/local") // This means URL's start with /demo (after Application path)
+public class UnidadeDeMedidaController  {
+	
+	long ID_NAO_ENCONTRADO = -1;
+
+    @Autowired
+    private ifrs.dev2.buyer.repositorios.UnidadeDeMedidaRepository repositorio;
+
+    @PostMapping(
+            value = "salvar"
+            , consumes = {MediaType.APPLICATION_JSON_VALUE}
+            , produces = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    public @ResponseBody
+    UnidadeDeMedida Salvar(@RequestBody ifrs.dev2.buyer.dados.UnidadeDeMedida entidade) {
+
+        repositorio.save(entidade);
+
+        return entidade;
+    }
+}
