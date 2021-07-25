df3b1fd199d2579d0042bb272f460cc393a482df
Fernando Rodrigues <34346597+nandolrs@users.noreply.github.com>
2021-07-25 02:50:54 -0300

    .

--- src/main/java/ifrs/dev2/buyer/repositorios/ReceitaRepository.java
+++ src/main/java/ifrs/dev2/buyer/repositorios/ReceitaRepository.java
@@ -12,15 +12,30 @@ import java.util.List;
 
 public interface ReceitaRepository extends CrudRepository<Receita, Long> {
 
+    @Query("select r from Receita r where r.produto.id <> 0")
+    List<Receita> findTudo();
+
     List<Receita> findByNomeContaining(String nome);
 
     @Query("select r from Receita r where r.produto.id = :produtoId")
     List<Receita> findByProdutoId(@Param("produtoId") Long produtoId);
 
     @Query("select r from Receita r where r.produto.id = :materialId")
-    List<Receita> findByMaterialId(@Param("materialId") Long materialId);
+    List<Receita> findByMaterialId1(@Param("materialId") Long materialId);
 
-    @Query("select r from Receita r where r.produto.id <> 0")
-    List<Receita> findTudo();
+
+    @Query(value = "select \n" +
+            "r.*\n" +
+            "from dbbuyer.receita r\n" +
+            "where r.id <> 0 -- 998\n" +
+            "and exists\n" +
+            "(\n" +
+            "\tselect 1 \n" +
+            "    from dbbuyer.receita_material rm\n" +
+            "    where rm.receita_id=r.id\n" +
+            "\tand rm.material_id = 49 -- laranja\n" +
+            ");\n"
+            , nativeQuery = true)
+    List<Receita> findByMaterialId(@Param("materialId") Long materialId);
 
 }
