039c032518393847bb2fa48628988e4c3a15e37c
theleozera <lpoliveira132@mail.com>
2021-07-25 20:48:11 -0300

    embalagem e unidade de medida response

--- src/main/java/ifrs/dev2/buyer/api/UnidadeMedidaController.java
+++ src/main/java/ifrs/dev2/buyer/api/UnidadeMedidaController.java
@@ -1,20 +1,22 @@
 package ifrs.dev2.buyer.api;
 
-import ifrs.dev2.buyer.dados.Classe;
-import ifrs.dev2.buyer.dados.DadoInterface;
-import ifrs.dev2.buyer.dados.Produto;
-import ifrs.dev2.buyer.dados.UnidadeMedida;
+import ifrs.dev2.buyer.dados.*;
+import ifrs.dev2.buyer.erros.ErroBase;
+import ifrs.dev2.buyer.erros.ErroItem;
+import ifrs.dev2.buyer.respostas.UnidadeMedidaResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.Id;
 import javax.print.attribute.standard.Media;
+import java.util.Comparator;
 import java.util.List;
 
 @RestController
-@RequestMapping(path="/api/UnidadeMedida")
+@RequestMapping(path="/api/unidademedida")
 public class UnidadeMedidaController  {
 
     long ID_NAO_ENCONTRADO = -1;
@@ -35,16 +37,41 @@ public class UnidadeMedidaController  {
         return entidade;
     }
 //responsive
-    @GetMapping(
-            value = "pesquisar"
-            , produces = {MediaType.APPLICATION_JSON_VALUE}
-    )
-    public @ResponseBody
-    List<UnidadeMedida> Pesquisar(@RequestParam String nome) {
+@GetMapping(
+        value = "pesquisar"
+        , produces = {MediaType.APPLICATION_JSON_VALUE}
+)
+public @ResponseBody
+UnidadeMedidaResponse Pesquisar(@RequestHeader HttpHeaders headers, @RequestParam String nome)
+{
+    try
+    {
+        List<UnidadeMedida> retorno = null;
 
-        List<UnidadeMedida> retorno = repositorio.findByNomeContaining(nome);
+        if(nome.length() > 0)
+        {
+            retorno = repositorio.findByNomeContaining(nome);
+        }
+        else
+        {
+            retorno = repositorio.findByNomeContaining(nome);
+        }
 
+        retorno.sort(Comparator.comparing(UnidadeMedida::getNome ));
+
+        return new UnidadeMedidaResponse( null,null,retorno);
+    }
+    catch(Exception e)
+    {
+        String msg = "vish";
+
+        ErroItem item = new ErroItem("",msg,-1L);
+        //ErroBase erroBase = new ErroBase(e);
+        ErroBase erroBase = new ErroBase(item);
+
+        UnidadeMedidaResponse retorno = new  UnidadeMedidaResponse(null, erroBase, null) ;
         return retorno;
+    }
     }
 
     @GetMapping(
