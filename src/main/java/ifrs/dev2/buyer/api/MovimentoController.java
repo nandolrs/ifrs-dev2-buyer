a502eda4f1917a0d7b17733d3c4f0c0fb5521200
theleozera <lpoliveira132@mail.com>
2021-08-16 20:05:44 -0300

    movimento consulta

--- src/main/java/ifrs/dev2/buyer/api/MovimentoController.java
+++ src/main/java/ifrs/dev2/buyer/api/MovimentoController.java
@@ -154,6 +154,7 @@ import java.util.List;
 
             Movimento retorno = new Movimento();
             try {
+                Long usuarioId = Cripto.Token2Usuario(headers,repositorioUsuarioAutenticador).getId();
                 retorno = repositorio.findById(id).get();
 
                 return new MovimentoResponse(retorno, null, null);
